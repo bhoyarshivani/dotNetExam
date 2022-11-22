03474fa4f0da762946fbccba1e5c05e87f140fcd
bhoyarshivani <115005094+bhoyarshivani@users.noreply.github.com>
2022-11-22 15:44:35 +0530

    Add files via upload

---
+++ dotnetexam1.1/first/DemoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using name;
+int a = 0;
+int b = int.Parse(args[0]);
+int c = int.Parse(args[1]);
+
+var d = new Computation(b);
+
+
+if (c == 0)
+{
+    Console.WriteLine($"The sum of Odd are : {d.GetSumOf(a, b, d.isOdd)}");
+}
+
+if (c == 1)
+{
+    Console.WriteLine($"The sum of Odd are : {d.GetSumOf(a, b, d.isEven)}");
+}
+if (c == 2)
+{
+    Console.WriteLine($"The sum of Odd are : {d.GetSumOf(a, b, d.isPrime)}");
+}
+
